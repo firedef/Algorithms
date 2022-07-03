2bdd3c21a3ee0b2be21165b21262f0542a4496f7
firedef <firedef2019@gmail.com>
2022-07-03 18:19:34 +0300

    GoodPairs

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,3 +1,3 @@
 ﻿using LeetCode.problems;
 
-Console.WriteLine(DeleteAndEarn.Solve(new[] {8,10,4,9,1,3,5,9,4,10}));
+Console.WriteLine(GoodPairs.Solve(new[] {1,2,3,1,1,3}));
---
+++ LeetCode/src/problems/GoodPairs.cs
@@ -0,0 +1,26 @@
+namespace LeetCode.problems; 
+
+public static class GoodPairs {
+    /// <summary>
+    /// https://leetcode.com/problems/number-of-good-pairs/
+    /// Given an array of integers nums, return the number of good pairs.
+    /// A pair (i, j) is called good if nums[i] == nums[j] and i < j
+    /// </summary>
+    public static int Solve(int[] nums) {
+        int len = nums.Length;
+        int c = 0;
+        for (int i = 0; i < len; i++)
+            c += CountGoodPairsFor(i, nums);
+        return c;
+    }
+
+    private static int CountGoodPairsFor(int i, int[] nums) {
+        int len = nums.Length;
+        int c = 0;
+        int v = nums[i];
+        for (int j = i + 1; j < len; j++)
+            if (nums[j] == v)
+                c++;
+        return c;
+    }
+}
